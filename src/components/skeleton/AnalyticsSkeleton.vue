359f09e9a85c2d1f001f3db2fcd6166627a66b5b
Yousouf Mustafa Muhammed <76212286+atheerweb@users.noreply.github.com>
2024-01-06 22:56:07 +0200

    skeleton (#10)

---
+++ src/components/skeleton/AnalyticsSkeleton.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+<template>
+  <v-container>
+    <v-skeleton-loader type="heading"></v-skeleton-loader>
+    <v-row>
+      <v-col v-for="n in 3" :key="n" cols="12" sm="4">
+        <v-skeleton-loader type="image"></v-skeleton-loader>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style scoped></style>
