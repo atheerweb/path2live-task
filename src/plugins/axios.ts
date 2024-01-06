b4451dc3de5eada55033ca9f58810f78d1a94f88
Yousouf Mustafa Muhammed <76212286+atheerweb@users.noreply.github.com>
2024-01-07 00:00:19 +0200

    axios change

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,4 +1,4 @@
 // Axios
 import axios from 'axios'
 // for sake of simplifying the review process I did not create env variables instead change to  http://localhost:3000 for local testing
-axios.defaults.baseURL = 'http://localhost:3000'
+axios.defaults.baseURL = 'https://my-json-server.typicode.com/atheerweb/path2live-task'
